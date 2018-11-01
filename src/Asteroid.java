51df5b35511984406af44efc282082eafabe520e
Summer Dawn <sumwriter@gmail.com>
2018-10-31 21:25:21 -0600

    Initial Asteroids

---
+++ src/Asteroid.java
@@ -0,0 +1,23 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Asteroid extends Polygon {
+
+	public Asteroid(Point[] shape, Point position, double rotation) {
+		super(shape, position, rotation);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void paint(Graphics brush, Color color) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
