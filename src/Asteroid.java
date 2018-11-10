eb6b2858038938510f1b260df7362e580fe8ff18
Summer Dawn <sumwriter@gmail.com>
2018-11-10 13:42:56 -0700

    Phase 2A of homeworkcomplete

--- src/Asteroid.java
+++ src/Asteroid.java
@@ -36,22 +36,18 @@ public class Asteroid extends Polygon {
 	@Override
 	public void move() {
 		position.x += Math.cos(Math.toRadians(rotation));
-		position.y += Math.sin(Math.toRadians(rotation));
+        if(position.x > Asteroids.SCREEN_WIDTH) {
+            position.x -= Asteroids.SCREEN_WIDTH;
+        } else if(position.x < 0) {
+            position.x += Asteroids.SCREEN_WIDTH;
+       }
 
-		// TO-DO
-		/**
-		 * Have asteroid move back on the screen once they go off the screen.
-		 *
-		 * You will do this by checking the value of position.x and position.y
-		 * and determine if they are outside of the bounds of the screen
-		 * specified by Asteroids.SCREEN_WIDTH and Asteroids.SCREEN_HEIGHT
-		 * If so, reposition the x and/or y coordinates.
-		 * 
-		 * i.e. if an asteroid moves off the right-side of the screen
-		 * have it re-appear on the left side of the screen.
-		 */
-
-		
+        position.y += Math.sin(Math.toRadians(rotation));
+        if(position.y > Asteroids.SCREEN_HEIGHT) {
+            position.y -= Asteroids.SCREEN_HEIGHT;
+        } else if(position.y < 0) {
+            position.y += Asteroids.SCREEN_HEIGHT;
+       }		
 
 	}
 }
