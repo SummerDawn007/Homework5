648b710209cc18a74eaf8bb9a338ca812e4d700d
Summer Dawn <sumwriter@gmail.com>
2018-11-10 13:57:17 -0700

    Added Ship

---
+++ src/Ship.java
@@ -0,0 +1,23 @@
+/**
+ *@author SummerDawn
+ *CMPT 202
+ *Fall 2018
+ */
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Ship extends Polygon {
+
+	@Override
+	public void paint(Graphics brush, Color color) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
